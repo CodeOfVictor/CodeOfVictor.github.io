7e367c7fd28bcbfa6625896b4f49cbd94afd6391
CodeOfVictor <correovictorb@gmail.com>
2021-11-01 12:31:29 +0100

    working on css

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,38 +1,50 @@
 import React from "react";
-import { Avatar, Button } from '@mui/material';
+import { Avatar, Button, Grid, Typography } from '@mui/material';
 import "./Presentation.css";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import EmailIcon from '@mui/icons-material/Email';
 
 class Presentation extends React.Component
 {
-
     render()
     {
         return (
-            <div id='Presentation'>
-                <div id="avatar">
+            <Grid container spacing={4}>
+                <Grid item xs={3}>
                     <Avatar
                         alt="Image cv"
-                        src="cvPhoto.jpg"
+                        src="" // cvPhoto.jpg
                         sx={{ width: 324, height: 324 }}
                     />
-                </div>
-                <div id="data">
-                    <Text>Víctor José Barneto Magen</Text>
-                    <Text>29/09/1998</Text>
-                    <Text>Zaragoza 50018</Text>
-                </div>
-                <div id="contact">
-                    <Button variant="outlined" onClick={openLinkedin}>Linkedin</Button>
-                    <Button variant="outlined" onClick={openGitHub}>Github</Button>
-                    <Button variant="outlined" onClick={openTwitter}>Twitter</Button>
-                    <Button variant="outlined" onClick={openMail}>Mail</Button>
-                </div>
-            </div>
+                </Grid>
+                <Grid item xs={7}>
+                    <Typography variant="body2">
+                        Víctor José Barneto Magen
+                    </Typography>
+                    <Typography variant="body2">
+                        29/09/1998
+                    </Typography>
+                    <Typography variant="body2">
+                        Zaragoza 50018
+                    </Typography>
+                    <Typography variant="body2">
+                        correovictorb@gmail.com
+                    </Typography>
+                </Grid>
+                <Grid item xs={2}>
+                    <Button variant="outlined" onClick={openLinkedIn}><LinkedInIcon/>LinkedIn</Button><br/>
+                    <Button variant="outlined" onClick={openGitHub}><GitHubIcon/>Github</Button><br/>
+                    <Button variant="outlined" onClick={openTwitter}><TwitterIcon/>Twitter</Button><br/>
+                    <Button variant="outlined" onClick={openMail}><EmailIcon/>Mail</Button>
+                </Grid>
+            </Grid>
             );
     }
 }
 
-function openLinkedin()
+function openLinkedIn()
 {
     var win = window.open("https://www.linkedin.com/in/v%C3%ADctor-jos%C3%A9/", 'linkedin');
     win.focus();
@@ -52,7 +64,7 @@ function openTwitter()
 
 function openMail()
 {
-    var win = window.open("correovictorb@gmail.com", 'mail');
+    var win = window.open("mailto:correovictorb@gmail.com", 'mail');
     win.focus();
 }
 
