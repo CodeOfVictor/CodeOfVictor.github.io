97f59026c85c143d07f57c1f60a8f96d633e15e6
CodeOfVictor <correovictorb@gmail.com>
2021-11-02 01:07:53 +0100

    working on paddling ... next commit theme and font

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Presentation from './components/Presentation.js';
+import Presentation from './components/Presentation';
 
 function App() {
   return (
--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,71 +1,63 @@
 import React from "react";
 import { Avatar, Button, Grid, Typography } from '@mui/material';
-import "./Presentation.css";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import EmailIcon from '@mui/icons-material/Email';
+import { makeStyles } from '@mui/styles';
 
-class Presentation extends React.Component
-{
-    render()
+const useStyles = makeStyles({
+    textInfo:
     {
-        return (
-            <Grid container spacing={4}>
-                <Grid item xs={3}>
-                    <Avatar
-                        alt="Image cv"
-                        src="" // cvPhoto.jpg
-                        sx={{ width: 324, height: 324 }}
-                    />
-                </Grid>
-                <Grid item xs={7}>
-                    <Typography variant="body2">
-                        Víctor José Barneto Magen
-                    </Typography>
-                    <Typography variant="body2">
-                        29/09/1998
-                    </Typography>
-                    <Typography variant="body2">
-                        Zaragoza 50018
-                    </Typography>
-                    <Typography variant="body2">
-                        correovictorb@gmail.com
-                    </Typography>
-                </Grid>
-                <Grid item xs={2}>
-                    <Button variant="outlined" onClick={openLinkedIn}><LinkedInIcon/>LinkedIn</Button><br/>
-                    <Button variant="outlined" onClick={openGitHub}><GitHubIcon/>Github</Button><br/>
-                    <Button variant="outlined" onClick={openTwitter}><TwitterIcon/>Twitter</Button><br/>
-                    <Button variant="outlined" onClick={openMail}><EmailIcon/>Mail</Button>
-                </Grid>
-            </Grid>
-            );
+        paddingTop: 6
+    },
+    buttonInfo:
+    {
+        marginTop: '10px !important',
+        width: '7rem'
     }
-}
+});
 
-function openLinkedIn()
-{
-    var win = window.open("https://www.linkedin.com/in/v%C3%ADctor-jos%C3%A9/", 'linkedin');
-    win.focus();
-}
+const Presentation = () => {
 
-function openGitHub()
-{
-    var win = window.open("https://github.com/CodeOfVictor", 'github');
-    win.focus();
-}
+    const classes = useStyles();
 
-function openTwitter()
-{
-    var win = window.open("https://twitter.com/CodeOfVictor", 'twitter');
-    win.focus();
-}
-
-function openMail()
-{
-    var win = window.open("mailto:correovictorb@gmail.com", 'mail');
-    win.focus();
+    const openLink = (src, name) => {
+        var win = window.open(src, name);
+        win.focus();
+    }
+    
+    return (
+        <Grid container spacing={4} justifyContent={'center'} alignItems={'center'}>
+            <Grid container justifyContent={'center'} item xs={2}>
+                <Avatar
+                    alt='Image cv'
+                    src='' // cvPhoto.jpg
+                    sx={{ width: 150, height: 150 }}
+                />
+            </Grid>
+            <Grid item xs={8}>
+                <Typography className={classes.textInfo}>
+                    Víctor José B.M.
+                </Typography>
+                <Typography className={classes.textInfo}>
+                    29/09/1998
+                </Typography>
+                <Typography className={classes.textInfo}>
+                    Zaragoza 50018
+                </Typography>
+                <Typography className={classes.textInfo}>
+                    correovictorb@gmail.com
+                </Typography>
+            </Grid>
+            <Grid item xs={2}>
+                <Button variant="outlined" className={classes.buttonInfo} onClick={() => openLink('https://www.linkedin.com/in/v%C3%ADctor-jos%C3%A9/', 'linkedin')}><LinkedInIcon/>LinkedIn</Button><br/>
+                <Button variant="outlined" className={classes.buttonInfo} onClick={() => openLink('https://github.com/CodeOfVictor', 'github')}><GitHubIcon/>Github</Button><br/>
+                <Button variant="outlined" className={classes.buttonInfo} onClick={() => openLink('https://github.com/CodeOfVictor', 'twitter')}><TwitterIcon/>Twitter</Button><br/>
+                <Button variant="outlined" className={classes.buttonInfo} onClick={() => openLink('mailto:correovictorb@gmail.com', 'email')}><EmailIcon/>Mail</Button>
+            </Grid>
+        </Grid>
+    );
 }
 
 export default Presentation;
