1e390eb376c26b98eb1cc018815becc2dd0a08e6
CodeOfVictor <60798818+CodeOfVictor@users.noreply.github.com>
2023-07-26 18:19:56 +0200

    responsive imgs and grids in phone

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,82 +20,80 @@ const Projects = () => {
     const { t } = useTranslation();
     
     return (
-        <Grid id='projects' container spacing={4} justifyContent={'center'} alignItems={'center'} style={{marginTop: 18}}>
-            <Grid container justifyContent={'center'} item xs={12} style={{marginTop: 18}}>
-                <Typography className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 32}}>
-                    {t('projects.title')}
+        <Grid id='projects' container spacing={4} justifyContent={'center'} alignItems={'center'} style={{ marginTop: 18 }}>
+            <Grid container justifyContent={'center'} item xs={12} style={{ marginTop: 18 }}>
+                <Typography className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 32 }}>
+                {t('projects.title')}
                 </Typography>
             </Grid>
 
-
-            <Grid item xs={7}>
-                <Typography  className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 36, marginBottom: 52}}>
-                    {t('projects.title1')}
+            {/* Primer proyecto */}
+            <Grid item xs={12} sm={7}>
+                <Typography className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 36, marginBottom: 52 }}>
+                {t('projects.title1')}
                 </Typography>
-                <Typography  className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22}}>
-                    {t('projects.subtitle1-1')}
+                <Typography className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22 }}>
+                {t('projects.subtitle1-1')}
                 </Typography>
-                <Typography  className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22}}>
-                    {t('projects.subtitle1-2')}
+                <Typography className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22 }}>
+                {t('projects.subtitle1-2')}
                 </Typography>
-                <Typography  className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22}}>
-                    {t('projects.subtitle1-3')}
+                <Typography className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22 }}>
+                {t('projects.subtitle1-3')}
                 </Typography>
             </Grid>
-            <Grid item xs={5} style={{display: "flex", justifyContent: "left"}}>
-                <img src='VideogameShopTycoon.gif' width="75%" height="75%" style={{maxWidth: '100%'}} />
-            </Grid>
-
-
-            <Grid item xs={5} style={{display: "flex", justifyContent: "right"}}>
-                <img src='CodeLyokoVR.gif' style={{maxWidth: '100%'}} />
-            </Grid>
-            <Grid item xs={7}>
-                <Typography  className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 36, marginBottom: 52}}>
-                    {t('projects.title2')}
-                </Typography>
-                <Typography  className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22}}>
-                    {t('projects.subtitle2-1')} 
-                </Typography>
-                <Typography  className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22}}>
-                    {t('projects.subtitle2-2')}
-                </Typography>
-                <Typography  className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22}}>
-                    {t('projects.subtitle2-3')}
-                </Typography>
+            <Grid item xs={12} sm={5} style={{ display: "flex", justifyContent: "center" }}>
+                <img src='VideogameShopTycoon.gif' style={{ maxWidth: '100%' }} />
             </Grid>
 
+            {/* Segundo proyecto */}
+            <Grid item xs={12} sm={5} style={{ display: "flex", justifyContent: "center" }}>
+                <img src='CodeLyokoVR.gif' style={{ maxWidth: '100%' }} />
+            </Grid>
+            <Grid item xs={12} sm={7}>
+                <Typography className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 36, marginBottom: 52 }}>
+                {t('projects.title2')}
+                </Typography>
+                <Typography className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22 }}>
+                {t('projects.subtitle2-1')}
+                </Typography>
+                <Typography className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22 }}>
+                {t('projects.subtitle2-2')}
+                </Typography>
+                <Typography className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22 }}>
+                {t('projects.subtitle2-3')}
+                </Typography>
+            </Grid>
 
-            <Grid item xs={7}>
-                <Typography  className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 36, marginBottom: 52}}>
-                    {t('projects.title3')}
+            {/* Tercer proyecto */}
+            <Grid item xs={12} sm={7}>
+                <Typography className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 36, marginBottom: 52 }}>
+                {t('projects.title3')}
                 </Typography>
-                <Typography  className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22}}>
-                    {t('projects.subtitle3-1')}
+                <Typography className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22 }}>
+                {t('projects.subtitle3-1')}
                 </Typography>
-                <Typography  className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22}}>
-                    {t('projects.subtitle3-2')}
+                <Typography className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22 }}>
+                {t('projects.subtitle3-2')}
                 </Typography>
             </Grid>
-            <Grid item xs={5} style={{display: "flex", justifyContent: "left"}}>
-                <img src='FarmProject.gif' style={{maxWidth: '100%'}} />
+            <Grid item xs={12} sm={5} style={{ display: "flex", justifyContent: "center" }}>
+                <img src='FarmProject.gif' style={{ maxWidth: '100%' }} />
             </Grid>
 
-
-            <Grid item xs={5}>
-            </Grid>
-            <Grid item xs={7}>
-                <Typography  className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 36, marginBottom: 52}}>
-                    {t('projects.title4')}
+            {/* Cuarto proyecto */}
+            <Grid item xs={12} sm={7}>
+                <Typography className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 36, marginBottom: 52 }}>
+                {t('projects.title4')}
                 </Typography>
-                <Typography  className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22}}>
-                    {t('projects.subtitle4-1')}
+                <Typography className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22 }}>
+                {t('projects.subtitle4-1')}
                 </Typography>
-                <Typography  className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22}}>
-                    {t('projects.subtitle4-2')}
+                <Typography className={classes.textInfo} style={{ fontFamily: "Montserrat", fontSize: 22 }}>
+                {t('projects.subtitle4-2')}
                 </Typography>
             </Grid>
-        </Grid>
+            </Grid>
     );
 }
 
